2a7521a544e7d5625c5b873bca939c1ead1b17c3
mocarafee <harif.rp05@gmail.com>
2020-06-26 17:50:02 +0900

    add removepackages list

--- Android.mk
+++ Android.mk
@@ -50,7 +50,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX Jelly GoogleTTS talkback PixelLiveWallpaperPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AudioFX Jelly GoogleTTS talkback PixelLiveWallpaperPrebuilt CalendarGooglePrebuilt talkback Photos SafetyHubPrebuilt TipsPrebuilt NgaResources RecorderPrebuilt arcore
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
